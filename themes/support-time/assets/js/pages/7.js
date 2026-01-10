e07a52dc248a21e783e56789d2e9b91500becb88
Stanislaw Timofeew <stastimofeew98@gmail.com>
2026-01-11 02:38:47 +0300

    dev platforms section on home page

--- themes/support-time/assets/js/pages/7.js
+++ themes/support-time/assets/js/pages/7.js
@@ -1,9 +1,23 @@
 import Swiper from "swiper";
+import { Navigation, Pagination } from "swiper/modules";
 
 const platformsSection = document.querySelector(".platforms");
 if (platformsSection) {
     const swiper = new Swiper(".swiper-content", {
-        slidesPerView: 1,
-        spaceBetween: 10,
+        slidesPerView: 3,
+        spaceBetween: 0,
+        centeredSlides: true,
+        loop: true,
+        speed: 1000,
+        grabCursor: true,
+        modules: [Navigation, Pagination],
+        navigation: {
+            nextEl: ".swiper-next",
+            prevEl: ".swiper-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            type: "bullets",
+        },
     });
 }
