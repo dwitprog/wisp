75b57c0b9322c0decf4bab483b0626096a8302c7
Stanislaw Timofeew <stastimofeew98@gmail.com>
2026-01-13 21:45:49 +0300

    adaptive footer

--- themes/support-time/my-get-files/pages/7.php
+++ themes/support-time/my-get-files/pages/7.php
@@ -502,13 +502,15 @@
                 <div class="item">
                     <p class="title">
                         How long does it take to develop a website?
-
                     </p>
-                    <p class="desc">
-                        We develop an online marketing strategy to reach your business goals. The result: you know how
-                        to establish or grow your online presence and how to measure efficiency.
-                    </p>
-                    <a href="#" class="btn">learn more</a>
+                    <div class="content">
+                        <p class="desc">
+                            We develop an online marketing strategy to reach your business goals. The result: you know
+                            how
+                            to establish or grow your online presence and how to measure efficiency.
+                        </p>
+                        <a href="#" class="btn">learn more</a>
+                    </div>
                 </div>
                 <div class="item active">
                     <p class="title">
@@ -516,42 +518,54 @@
                         target or context?
 
                     </p>
-                    <p class="desc">
-                        We develop an online marketing strategy to reach your business goals. The result: you know how
-                        to establish or grow your online presence and how to measure efficiency.
-                    </p>
-                    <a href="#" class="btn">learn more</a>
+                    <div class="content">
+                        <p class="desc">
+                            We develop an online marketing strategy to reach your business goals. The result: you know
+                            how
+                            to establish or grow your online presence and how to measure efficiency.
+                        </p>
+                        <a href="#" class="btn">learn more</a>
+                    </div>
                 </div>
                 <div class="item">
                     <p class="title">
                         How quickly will I see the results
                         of the promotion?
                     </p>
-                    <p class="desc">
-                        We develop an online marketing strategy to reach your business goals. The result: you know how
-                        to establish or grow your online presence and how to measure efficiency.
-                    </p>
-                    <a href="#" class="btn">learn more</a>
+                    <div class="content">
+                        <p class="desc">
+                            We develop an online marketing strategy to reach your business goals. The result: you know
+                            how
+                            to establish or grow your online presence and how to measure efficiency.
+                        </p>
+                        <a href="#" class="btn">learn more</a>
+                    </div>
                 </div>
                 <div class="item">
                     <p class="title">
                         Why are competitors cheaper?
                     </p>
-                    <p class="desc">
-                        We develop an online marketing strategy to reach your business goals. The result: you know how
-                        to establish or grow your online presence and how to measure efficiency.
-                    </p>
-                    <a href="#" class="btn">learn more</a>
+                    <div class="content">
+                        <p class="desc">
+                            We develop an online marketing strategy to reach your business goals. The result: you know
+                            how
+                            to establish or grow your online presence and how to measure efficiency.
+                        </p>
+                        <a href="#" class="btn">learn more</a>
+                    </div>
                 </div>
                 <div class="item">
                     <p class="title">
                         Why should you choose us?
                     </p>
-                    <p class="desc">
-                        We develop an online marketing strategy to reach your business goals. The result: you know how
-                        to establish or grow your online presence and how to measure efficiency.
-                    </p>
-                    <a href="#" class="btn">learn more</a>
+                    <div class="content">
+                        <p class="desc">
+                            We develop an online marketing strategy to reach your business goals. The result: you know
+                            how
+                            to establish or grow your online presence and how to measure efficiency.
+                        </p>
+                        <a href="#" class="btn">learn more</a>
+                    </div>
                 </div>
             </div>
         </div>
